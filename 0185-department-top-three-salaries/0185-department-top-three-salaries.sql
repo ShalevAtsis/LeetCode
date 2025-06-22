0fccec305fffd43fa210b7118a110117899b5513
Shalev Atsis <shalevatsis@gmail.com>
2025-06-22 15:12:50 +0300

    1295 ms (22.25%) 0B (100%) 06-22-2025 0185-department-top-three-salaries Hard .sql

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+
+
+SELECT
+    d.name AS Department,
+    e1.name AS Employee,
+    e1.salary AS Salary
+FROM Employee AS e1
+JOIN Department AS d ON e1.departmentId = d.id
+WHERE (
+    SELECT COUNT(DISTINCT e2.salary)
+    FROM Employee AS e2
+    WHERE e1.departmentId = e2.departmentId
+        AND e1.salary < e2.salary
+) < 3;
